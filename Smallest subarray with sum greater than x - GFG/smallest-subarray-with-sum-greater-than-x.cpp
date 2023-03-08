0b00c9c02db5ab3d5a4ae61ef3e61506e5be8ba8
Harshit Srivastava <83102075+harshit2024@users.noreply.github.com>
2023-03-08 17:40:54 +0530

    Added solution - LeetHub

---
+++ Smallest subarray with sum greater than x - GFG/smallest-subarray-with-sum-greater-than-x.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+  public:
+
+    int smallestSubWithSum(int arr[], int n, int x)
+    {
+        // Your code goes here    
+        int left=0,res=INT_MAX,count=0;
+        
+        for(int i=0;i<n;i++){
+            count+=arr[i];
+            while(count>x){
+                res=min(res,i-left+1);
+                count-=arr[left++];
+            }
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n,x;
+		cin>>n>>x;
+		int a[n];
+		for(int i=0;i<n;i++)
+		cin>>a[i];
+		Solution obj;
+		cout<<obj.smallestSubWithSum(a,n,x)<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
