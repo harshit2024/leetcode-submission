b9341323be83b4f180cc674c180326c2720796e2
Harshit Srivastava <83102075+harshit2024@users.noreply.github.com>
2023-04-04 10:32:19 +0530

    Time: 5 ms (31.83%), Space: 6.7 MB (72.27%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -24,12 +24,29 @@ public:
         return dp[target];
     
     }
-        
+      int solvetab(vector<int>& nums, int target) {
+          vector<unsigned int>dp(target+1,0);
+          dp[0]=1;
+          
+          for(int i=1;i<=target;i++)
+              {
+                  for(int j=0;j<nums.size();j++){
+                      if(i-nums[j]>=0){
+                          dp[i]+=dp[i-nums[j]];
+                      }
+                      
+                  
+                }
+               
+             }
+          return dp[target];
+          
+      } 
     int combinationSum4(vector<int>& nums, int target) {
         
-        vector<int>dp(target+1,-1);
+        //vector<int>dp(target+1,-1);
        
-        return  solve(nums,target,dp);
+        return  solvetab(nums,target);
         
     }
 };
