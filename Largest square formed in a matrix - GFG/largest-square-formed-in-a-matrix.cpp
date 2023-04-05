ca4ef3c2212435fb9588d50210ace3afb4a18c45
Harshit Srivastava <83102075+harshit2024@users.noreply.github.com>
2023-04-05 09:26:28 +0530

    Added solution - LeetHub

--- Largest square formed in a matrix - GFG/largest-square-formed-in-a-matrix.cpp
+++ Largest square formed in a matrix - GFG/largest-square-formed-in-a-matrix.cpp
@@ -10,16 +10,35 @@ using namespace std;
 class Solution{
 public:
 
-    int solve(vector<vector<int>> &mat,int i,int j,int &maxi,vector<vector<int>> &dp){
+int solve(vector<vector<int>> &mat,int i,int j,int &maxi){
+        if(i>=mat.size()||j>=mat[0].size()){
+            return 0;
+        }
+       
+        int right=solve(mat,i,j+1,maxi);
+        int diagonal=solve(mat,i+1,j+1,maxi);
+        int down=solve(mat,i+1,j,maxi);
+        int ans=0;
+        if(mat[i][j]==1){
+            ans=1+min(right,min(diagonal,down));
+            maxi=max(maxi,ans);
+        
+            return ans;
+        }
+        else{
+            return 0;
+        }
+    }
+    int solvetab(vector<vector<int>> &mat,int i,int j,int &maxi,vector<vector<int>> &dp){
         if(i>=mat.size()||j>=mat[0].size()){
             return 0;
         }
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
-        int right=solve(mat,i,j+1,maxi,dp);
-        int diagonal=solve(mat,i+1,j+1,maxi,dp);
-        int down=solve(mat,i+1,j,maxi,dp);
+        int right=solvetab(mat,i,j+1,maxi,dp);
+        int diagonal=solvetab(mat,i+1,j+1,maxi,dp);
+        int down=solvetab(mat,i+1,j,maxi,dp);
         
         if(mat[i][j]==1){
             dp[i][j]=1+min(right,min(diagonal,down));
@@ -31,11 +50,34 @@ public:
             return dp[i][j]=0;
         }
     }
+    
+    int solvetab2(vector<vector<int>> &mat,int n,int m,int &maxi){
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        
+        for(int i=n-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                 int right=dp[i][j+1];
+                 int diagonal=dp[i+1][j+1];
+                 int down=dp[i+1][j];
+        
+        if(mat[i][j]==1){
+            dp[i][j]=1+min(right,min(diagonal,down));
+            maxi=max(maxi,dp[i][j]);
+        
+           
+        }
+        else{
+            dp[i][j]=0;
+        }
+            }
+        }
+        return dp[0][0];
+    }
     int maxSquare(int n, int m, vector<vector<int>> mat){
         // code here
         vector<vector<int>>dp(n,vector<int>(m,-1));
         int maxi=0;
-        solve(mat,0,0,maxi,dp);
+        solvetab(mat,0,0,maxi,dp);
         return  maxi;
     }
 };
